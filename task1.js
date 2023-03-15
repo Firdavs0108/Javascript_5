8ac89cba1a2e1d2016378fe1fe6886bd67e2e1e8
Nazarov Firdavs <fredrickuzb55@gmail.com>
2023-03-15 23:42:59 +0900

    first commit

---
+++ task1.js
@@ -0,0 +1,119 @@
+
+
+// function declaration
+
+// function example () {
+//     return 'how are you'
+// }
+
+// console.log(example());
+
+
+//=================================
+
+
+// function expression 
+
+// let example = function (){
+//     return 'hey, how are you'
+// }
+
+// console.log(example());
+
+
+//=====================================
+
+// array function
+
+// let example = () => {
+//     return 'hey';
+// }
+
+// console.log(example());
+
+
+//=====================================
+
+//parametr = o'zgaruvchi / argument=qiymat
+
+// function person(ism, yosh, millat) {
+//     console.log('Ism:' + ism + '  ' + 'Yosh:' + yosh + '  ' + 'Millat:' + millat) 
+// }
+
+// person('Firdavs', '22' , 'Uzbek');
+// person('Firdavs', '22', 'Russian');
+// person('Firdavs', '22' , 'American');
+
+
+// Yuqori va pastdagi bir xil lekin usullar xar xil ||
+
+// function person(ism = 'Default ism', yosh = 'Default yosh', millat = 'Default millat') {
+//     return ( 'Ismi: ' + ism + '  ' + 'Yoshi: ' + yosh + '  ' + 'Millati: ' + millat ) 
+// }
+
+// console.log(person( ));
+// console.log(person('Firdavs', '22', 'Uzbek'));
+// console.log(person('Firdavs', '22', 'Uzbek'));
+// console.log(person('Firdavs', '22', 'Uzbek'));
+
+
+//============================
+//Homework example task
+
+//============================
+//MathPow
+
+// function misolPow (son, daraja) {
+//     return son ** daraja
+// }
+
+// console.log(misolPow(3,2));
+
+
+//==============================
+//MathSqrt
+
+// function misolSqrt (son) {
+//     return son ** (1/2)
+// }
+
+// console.log(misolSqrt(81));
+
+
+//==============================
+// Math.abs 
+
+// let= modul = (a) => {
+//      if(a>0 || a==0)
+//      return a
+
+//      else {
+//         return a-a-a
+//      }
+// }
+
+// console.log(modul(-20));
+
+
+//================================
+// Math.round
+
+// let= round = (a) => {
+//      if(a>0,5 || a==a)
+//      return a
+
+//      else {
+//         return a+1
+//      }
+// }
+
+// console.log(round(20));
+
+// let = round = a => {
+//     return a
+// }
+
+// console.log( Number.parseInt, round (20,666));
+
+
+
---
+++ task2.js
@@ -0,0 +1,56 @@
+
+
+//============================
+//MathPow
+
+// function misolPow (son, daraja) {
+//     return son ** daraja
+// }
+
+// console.log(misolPow(3,2));
+
+
+//==============================
+//MathSqrt
+
+// function misolSqrt (son) {
+//     return son ** (1/2)
+// }
+
+// console.log(misolSqrt(81));
+
+
+//==============================
+// Math.abs 
+
+// let= modul = (a) => {
+//      if(a>0 || a==0)
+//      return a
+
+//      else {
+//         return a-a-a
+//      }
+// }
+
+// console.log(modul(-20));
+
+
+//=============================
+
+// Math.floor
+
+// function floor (son) {
+//         return son 
+//     }
+    
+//     console.log( Number.parseInt, floor (3,800));
+
+//==============================
+
+//Math.round
+
+function round (son) {
+    return son 
+}
+
+console.log( round (3,800).toFixed(1) );
